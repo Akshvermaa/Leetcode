25acfbc052b9da6343fc3c0640457d8737001ed8
Akshvermaa <akshverma218@gmail.com>
2026-07-25 23:49:28 +0530

    Time: 1 ms (100%), Space: 42.4 MB (92.77%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int n) {
+        int[]freq=new int[10];
+        while(n>0){
+            freq[n%10]++;
+            n/=10;
+        }
+        for(int i=9;i>=0;i--){
+            if(freq[i]>=2){
+                return i*i;
+
+            }
+            if(freq[i]==1){
+                for(int j=i-1;j>=0;j--){
+                    if(freq[j]>0){
+                        return i*j;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
